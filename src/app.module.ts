db2cb0078a1de2e4331cfc800834b68b709792fc
Luiz Henrique <luizhjramos@outlook.com>
2023-07-10 20:08:12 -0300

    feat: initialize products module with nestjs boilerplate

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ProductsModule } from './products/products.module';
 
 // The @Module() decorator provides metadata that Nest makes use of to organize the application structure.
 @Module({
-  imports: [CategoriesModule, PrismaModule],
+  imports: [CategoriesModule, PrismaModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
