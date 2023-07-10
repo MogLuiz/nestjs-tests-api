37763eb13045abdd93b089174fa18cc09cdcf817
Luiz Henrique <luizhjramos@outlook.com>
2023-07-10 06:17:54 -0300

    chore: initialize prisma module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 // The @Module() decorator provides metadata that Nest makes use of to organize the application structure.
 @Module({
-  imports: [CategoriesModule],
+  imports: [CategoriesModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
